18480b552374043eb43ea036ac275a7c9c3e2b00
Danila Egorov <c4nsero@mail.ru>
2021-10-05 11:04:29 +0300

    + middleOfTheLinkedList

---
+++ middleOfTheLinkedList_05_10_21/middleOfTheLinkedList_05_10_21.go
@@ -0,0 +1,17 @@
+package main
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func middleNode(head *ListNode) *ListNode {
+	slow, fast := head, head
+	for fast != nil && fast.Next != nil {
+		slow, fast = slow.Next, fast.Next.Next
+	}
+	return slow
+}
+
+func main() {
+}
