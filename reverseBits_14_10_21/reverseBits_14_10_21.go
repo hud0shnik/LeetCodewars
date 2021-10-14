220b49c93c1bf2bf04c43645d3b6752eda0bbc52
Danila Egorov <c4nsero@mail.ru>
2021-10-14 11:53:26 +0300

    + reverseBits

---
+++ reverseBits_14_10_21/reverseBits_14_10_21.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func reverseBits(num uint32) uint32 {
+	var result uint32
+	for i := 0; i < 32; i++ {
+		result = result<<1 + num&1
+		num >>= 1
+	}
+	return result
+}
+
+func main() {
+	fmt.Println(reverseBits(43261596)) // = 00000010100101000001111010011100
+}
