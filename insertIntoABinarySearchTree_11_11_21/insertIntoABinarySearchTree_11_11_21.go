7cc8b8f321f4ff1eccafadc9003ddee21023d9b6
Danila Egorov <c4nsero@mail.ru>
2021-11-11 12:32:07 +0300

    + insertIntoABinarySearchTree

---
+++ insertIntoABinarySearchTree_11_11_21/insertIntoABinarySearchTree_11_11_21.go
@@ -0,0 +1,21 @@
+package main
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func insertIntoBST(root *TreeNode, val int) *TreeNode {
+	if root == nil {
+		return &TreeNode{Val: val}
+	}
+
+	if root.Val > val {
+		root.Left = insertIntoBST(root.Left, val)
+		return root
+	}
+
+	root.Right = insertIntoBST(root.Right, val)
+	return root
+}
