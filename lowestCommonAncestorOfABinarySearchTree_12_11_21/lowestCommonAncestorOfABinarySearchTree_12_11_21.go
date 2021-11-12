4d756038d76ebc8f7e257258db69d0c2ba078796
Danila Egorov <c4nsero@mail.ru>
2021-11-12 07:12:43 +0300

    + lowestCommonAncestorOfABinarySearchTree

---
+++ lowestCommonAncestorOfABinarySearchTree_12_11_21/lowestCommonAncestorOfABinarySearchTree_12_11_21.go
@@ -0,0 +1,18 @@
+package main
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func lowestCommonAncestor(root, p, q *TreeNode) *TreeNode {
+	if p.Val > root.Val && q.Val > root.Val {
+		return lowestCommonAncestor(root.Right, p, q)
+	}
+	if p.Val < root.Val && q.Val < root.Val {
+		return lowestCommonAncestor(root.Left, p, q)
+	}
+
+	return root
+}
